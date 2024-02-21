788cd0083600caaea8ebcfa11f07dad678605e3a
JustGaming6 <103388149+JustGaming6@users.noreply.github.com>
2024-02-21 21:10:00 +1300

    21/02

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -7,7 +7,7 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/selection.tscn")
 
 
 func _on_options_pressed():
