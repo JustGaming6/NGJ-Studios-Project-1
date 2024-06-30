dcb63ff4a9ce6f4d5f8d5732fe8e359b4009d585
JustGaming6 <justinmiller3186@gmail.com>
2024-07-01 10:54:23 +1200

    01/07

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -7,3 +7,7 @@ extends Node2D
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
+
+
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
